eb389f246bd89e11669b760985014c34720869eb
Evan Morisato <evanmorisato@yahoo.com>
2020-04-04 23:24:24 -0500

    removed request

--- liri.js
+++ liri.js
@@ -3,7 +3,6 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
-var request = require('request');
 var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
